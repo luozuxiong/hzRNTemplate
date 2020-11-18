b605a1e4ad1187890102e7ff61fa2ea21d7ae706
luozuxiong <625846782@qq.com>
2020-11-19 00:57:37 +0800

    passport

--- src/screens/passport/Index.tsx
+++ src/screens/passport/Index.tsx
@@ -1,33 +1,64 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { Tabs } from '@ant-design/react-native'
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, ImageBackground} from 'react-native';
+import {Tabs} from '@ant-design/react-native';
+const tabs = [
+  {title: 'First Tab'},
+  {title: 'Second Tab'},
+  {title: 'Third Tab'},
+];
+const style = {
+  paddingVertical: 40,
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 10,
+  backgroundColor: '#ddd',
+};
 export default class Passport extends Component {
-    tabs = [
-        {
-            title: '短信登陆',
-            value: 1
-        },
-        {
-            title: '密码登陆',
-            value: 2
-        }
-    ]
-    render() {
-        return (
-            <View style={styles.wrapper}>
-                <Tabs tabs={this.tabs}>
-                    <View>
-                        <Text>3</Text>
-                    </View>
-                    <View>
-                        <Text>2</Text>
-                    </View>
-                </Tabs>
+  tabs = [
+    {
+      title: '短信登陆',
+      value: 1,
+    },
+    {
+      title: '密码登陆',
+      value: 2,
+    },
+  ];
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <ImageBackground
+          resizeMode="contain"
+          style={styles.content}
+          imageStyle={{
+            alignSelf: 'flex-end',
+            resizeMode: 'contain',
+          }}
+          source={require('./../../../assets/images/xhdpi/login_head_image.png')}>
+          <Tabs tabs={this.tabs}>
+            <View>
+              <Text>Content of First Tab</Text>
             </View>
-        )
-    }
+            <View>
+              <Text>Content of Second Tab</Text>
+            </View>
+          </Tabs>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {}
+  wrapper: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    position: 'absolute',
+    height: 800,
+    backgroundColor: 'red',
+    bottom: 0,
+  },
 });
